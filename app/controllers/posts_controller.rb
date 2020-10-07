885d760b455044bbd57934f00dfa4d8370fb2679
uchiyama777 <ucchi_uchi@hotmail.co.jp>
2020-10-07 12:10:22 +0900

    メモ投稿機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,12 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(id: "DESC")
   end
+
   def create
-    Post.create(content: params[:content])
-    redirect_to action: :index
+    post = Post.create(content: params[:content], checked: false)
+    render json:{ post: post }
   end
+
   def checked
     binding.pry
     post = Post.find(params[:id])
@@ -14,6 +16,7 @@ class PostsController < ApplicationController
     else
       post.update(checked: true)
     end
+    
     item = Post.find(params[:id])
     render json: { post: item }
   end
